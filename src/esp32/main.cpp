f5f15a0fd4925c4ce6e047fc4ed499d4e4d0b0c3
Rafael Hipólito <64715008+RafaelHipolit@users.noreply.github.com>
2023-12-03 22:25:50 -0300

    Fix concatenation, remove LED OLED code
    fix char concatenation in string;
    Remove LED and OLDE code

--- src/esp32/main.cpp
+++ src/esp32/main.cpp
@@ -2,34 +2,15 @@
 #include <WiFi.h>
 #include <ArduinoWebsockets.h>
 #include <string.h>
-#include <LiquidCrystal.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
 
-#define WEB_SOCKET_SERVER "ws://[IPV4]:8080/"
-#define SSID "nome" //nome wife
-#define PASSWORD "senha" //senha wifi
-
-#define LCD_ON 1 //ativa codigo do lcd = 1; se nao 0
-#define OLED_ON 1 //ativa codigo do telinha oled = 1; se nao 0
-
-#define OLED_SCREEN_WIDTH 128 // OLED display width, in pixels
-#define OLED_SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define WEB_SOCKET_SERVER "ws://192.168.243.115:8080/"
+#define SSID "n" //nome wife
+#define PASSWORD "n" //senha wifi
 
 //#define RXp2 16
 //#define TXp2 17
 
 //conectar no pino RX_2 (pin 16) pra receber input de Serial2
-
-Adafruit_SSD1306 display(OLED_SCREEN_WIDTH, OLED_SCREEN_HEIGHT, &Wire, -1);
-//fonte: https://randomnerdtutorials.com/esp32-ssd1306-oled-display-arduino-ide/
-
-
-// Create An LCD Object. Signals: 
-//              [ RS, E , D4, D5, D6, D7 ]
-LiquidCrystal lcd(13, 12, 14, 27, 26, 25);
-//Fonte: https://deepbluembedded.com/esp32-lcd-display-16x2-without-i2c-arduino/
 
 websockets::WebsocketsClient socket;
 //fonte: https://www.youtube.com/watch?v=eFhOlZTFyr4
@@ -79,63 +60,23 @@ void handleEvent(websockets::WebsocketsEvent e, websockets::WSInterfaceString da
 
 }
 
-void LCDsetup(){
-	lcd.begin(16, 2);		   // Colunas, linhas. Use 16,2 para display LCD 16x2
-	lcd.clear();			   // Inicializa o display com uma tela vazia
-	lcd.setCursor(0, 0);	   // Posiciona o cursor na coluna 0, linha 0 (primeira linha)
-	lcd.print("Iniciando..."); // Escreve o texto desejado
-
-	lcd.setCursor(0, 1); // Posiciona o cursor para a coluna 0, linha 1 (segunda linha)
-	lcd.print("----");
-}
-
-void OLEDsetup(){
-	if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
-    Serial.println(F("SSD1306 allocation failed"));
-    //for(;;);
-  }
-  delay(2000);
-  display.clearDisplay();
-
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.println("iniciando...");
-  display.display(); //inicia a exibicao na tela
-}
-
 void setup()
-{
-	//conecta ao wifi e web socket
-	connectWiFi();
-    connectWebSocket();
-    socket.onMessage(handleMessage);
-    socket.onEvent(handleEvent);
-
-	if (LCD_ON)
-	{
-		LCDsetup();
-	}
-
-	if (OLED_ON)
-	{
-		OLEDsetup();
-	}	
+{	
 
 	Serial.begin(115200);
 
 	//Serial2.begin(9600, SERIAL_8N1, RXp2, TXp2);
 	Serial2.begin(9600);
 
-	//pinMode(15,INPUT);
-	//pinMode(2,OUTPUT);
-	//digitalWrite(2,LOW);
+	//conecta ao wifi e web socket
+	connectWiFi();
+    connectWebSocket();
+    socket.onMessage(handleMessage);
+    socket.onEvent(handleEvent);
 }
 
-int cont = 0;
-
 void loop()
-{
+{	
 
 	charRead = Serial2.read();
 	Serial.print("char = ");
@@ -149,7 +90,7 @@ void loop()
 		charRead = Serial2.read();		
 		while (charRead != '#')
 		{
-			stringAux += std::to_string(charRead);
+			stringAux += charRead;
 			charRead = Serial2.read();
 		}
 		numPassengers = std::stoi(stringAux);
@@ -158,62 +99,23 @@ void loop()
 		charRead = Serial2.read();
 		while (charRead != '#')
 		{
-			stringAux += std::to_string(charRead);
+			stringAux += charRead;
 			charRead = Serial2.read();
 		}
 		percentPowerFan = std::stoi(stringAux);
 
-		if (LCD_ON)
-		{
-			lcd.clear();
-			lcd.setCursor(0, 0);
-			lcd.print("Pessoas=");
-			lcd.setCursor(8, 0);
-			lcd.print(numPassengers, 10);
-			lcd.setCursor(0, 1);
-			lcd.print("Tensao%=");
-			lcd.setCursor(8, 0);
-			lcd.print(percentPowerFan, 10);
-		}
+		Serial.print("Pessoas=");
+		Serial.print(numPassengers);
+		Serial.print(";Ventoinha(%)=");
+		Serial.println(percentPowerFan);
 
-		if (OLED_ON)
-		{
-			display.clearDisplay();
-			display.setCursor(0, 0);
-			display.println("Passageiros=");
-			display.setCursor(0, 1);
-			display.println(numPassengers, 10);
-			display.setCursor(0, 2);
-			display.println("Tensao=");
-			display.setCursor(0, 3);
-			display.println(percentPowerFan, 10);
-		}
 
+		socket.send(std::to_string(numPassengers).c_str());
 	}
 	else if (charRead == 'd')
 	{
-		lcd.clear();
-		lcd.setCursor(0, 0);
-		lcd.print("Desligado");
-
-		display.clearDisplay();
-		display.setCursor(0, 0);
-  		display.println("Desligado");
+		socket.send("CONTADOR DESLIGADO");
 	}
-
-	/*
-	if (digitalRead(15))
-	{
-		Serial.println("PIN ON");
-		cont++;
-	}
-	display.clearDisplay();
-	display.setCursor(0, 0);
-	display.print("0123456789"); //10 char
-	display.setCursor(0, 16);
-	display.println(cont,10);
-	display.display();
-	*/
 
 
 	if(!connectedServer){
@@ -222,8 +124,6 @@ void loop()
     }
 
     socket.poll(); // chama os handleMessage e handleEvent
-
-    socket.send(std::to_string(numPassengers).c_str());
 
     delay(2000);
 }
